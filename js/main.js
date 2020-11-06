69a4d46665f76cf3d0571753f3c5e9f79286db12
Noah <noahhodge2@gmail.com>
2020-11-05 19:10:13 -0800

    Javascript navigation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-let articleCollection = document.getElementsByClassName("article")
+let articleCollection = document.getElementsByClassName("content")
 
-function moveHidden() {
+function moveHidden(id) {
     for (i = 0; i < articleCollection.length; i++) {
-        articleCollection[i].className += " hidden";
+        articleCollection[i].className += ' hidden';
     }
+
+   let activateElement = document.getElementsByClassName(id);
+   for (i = 0; i < activateElement.length; i ++) {
+    activateElement[i].className -= ' hidden';
+   }
+
 }
