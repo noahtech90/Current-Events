f6ebec79a8286346e0582ad9f1eaa89b4cb1dd55
Noah <noahhodge2@gmail.com>
2020-11-16 16:54:28 -0800

    Display Edits

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function moveHidden(id) {
     for (i = 0; i < articleCollection.length; i++) {
         articleCollection[i].className += ' hidden';
     }
-
+    
     var international_div = document.querySelector("." + id);
     international_div.className -= ' hidden';
 
