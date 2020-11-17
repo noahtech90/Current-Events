bc2966dcc3a47e6883fa6ba5f6d3006da2a8d503
Noah <noahhodge2@gmail.com>
2020-11-16 17:04:09 -0800

    Display function

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ function moveHidden(id) {
     for (i = 0; i < articleCollection.length; i++) {
         articleCollection[i].className += ' hidden';
     }
-    
-    var international_div = document.querySelector("." + id);
-    international_div.className -= ' hidden';
+
+    var div = document.querySelector("." + id);
+    console.log(div);
+    div.classList.replace(' hidden', '');
 
 }
